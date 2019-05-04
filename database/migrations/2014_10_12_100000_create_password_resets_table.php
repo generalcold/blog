e21f6d2f5848b1d11bba5ecbb3b129c50c32e7a3
zhangzhijun <751093720@qq.com>
2019-05-04 21:09:24 +0800

    个人博客系统

---
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+// slug：将文章标题转化为 URL 的一部分，以利于SEO
+// title：文章标题
+// content：文章内容
+// published_at：文章正式发布时间
+// deleted_at：用于支持软删除
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordResetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('password_resets');
+    }
+}
